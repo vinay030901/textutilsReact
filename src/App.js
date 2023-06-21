2fa717b372f0f5308a432b6434a00601480ae775
Vinay Kumar <champ.vk91@gmail.com>
2023-06-22 01:39:52 +0530

    textform

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+import Alert from './components/Alert';
+// import About from './components/About';
 // let names = "Vinay";
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
 
+  const showAlert = (message, type) => {
+    setAlert({ msg: message, type: type });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500)
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = "#042743";
+      showAlert('dark mode has been enabled', 'success');
+    }
+
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+      showAlert('light mode has been enabled', 'success');
+    }
+  };
   return (
+
     <>
-      <Navbar title="TextUtils" aboutText="About Us" />
+      <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        {/* <TextForm heading="Enter the text to analyze" /> */}
-        <About></About>
+        <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />
+        {/* <About></About> */}
       </div >
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,34 +5,47 @@ export default function TextForm(props) {
     const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to Uppercase", "success");
     }
     const handleLowClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to Lowercase", "success");
     }
     const handleChange = (event) => {
         setText(event.target.value);
     }
     const handleClearClick = (event) => {
         setText("");
+        props.showAlert("Text cleared", "success");
+    }
+    const handleCopy = () => {
+        var text = document.getElementById("textarea");
+        text.select();
+        text.setSelectionRange(0, 9999);
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Text copied to Clipboard", "success");
     }
     return (
         <>
-            <div className='container'>
+            <div className='container' style={{ color: props.mode === 'dark' ? 'white' : 'grey' }}>
                 <h1>{props.heading}</h1>
 
                 <div className="form-group mb-3">
-                    <textarea className="form-control" value={text} onChange={handleChange} id="exampleFormControlTextarea1" rows="10"></textarea>
+                    <textarea className="form-control" style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'grey' }} value={text} onChange={handleChange} id="textarea" rows="10"></textarea>
                 </div>
                 <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className='btn btn-primary mx-2' onClick={handleLowClick}>Convert to Lowercase</button>
                 <button className='btn btn-primary mx-2' onClick={handleClearClick}>Clear text</button>
+                <button className='btn btn-primary mx-2' onClick={handleCopy}>Copy text</button>
 
             </div>
-            <div className="containter my-5">
+            <div className="containter my-3" style={{ color: props.mode === 'dark' ? 'white' : 'grey' }}>
                 <h1>Your text summary</h1>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} minutes read</p>
+                <h2>Preview</h2>
+                <p>{text.length > 0 ? text : "Enter something in text area to preview it here"}</p>
             </div>
         </>
     )
